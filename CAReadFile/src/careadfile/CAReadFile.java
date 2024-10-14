7fb7f0fa89e8c98acf89a463fd6b1b3b430fb193
GustavoBritoL <sba24008@student.cct.ie>
2024-10-14 12:13:24 +0100

    First Commit
    - java project created

---
+++ CAReadFile/src/careadfile/CAReadFile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package careadfile;
+
+/**
+ *
+ * @author gustavobrito22icloud.com
+ */
+public class CAReadFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
