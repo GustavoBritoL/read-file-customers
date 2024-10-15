0e259b5a45e84d8ee4b7492b464395bd23d305cb
GustavoBritoL <sba24008@student.cct.ie>
2024-10-15 11:43:15 +0100

    Importing-classes-for-reader
    - classes imported from java.io to read file (BufferedReader, FileReader, IOException).
    -Defining filePath in order to use FileReader.
    -Try block to read each line of the file using readLine().
    - Catch handles any file operation failure.

--- CAReadFile/src/careadfile/CAReadFile.java
+++ CAReadFile/src/careadfile/CAReadFile.java
@@ -4,6 +4,11 @@
  */
 package careadfile;
 
+// classes imported from java.io to read file and handle errors 
+import java.io.BufferedReader; //Reads files, wraps FileReader
+import java.io.FileReader; //Reads files
+import java.io.IOException; //Handles exception and throw error message
+
 /**
  *
  * @author gustavobrito22icloud.com
@@ -15,6 +20,25 @@ public class CAReadFile {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+          // Defining the file path as string to be read 
+              String filePath = "/Users/gustavobrito22icloud.com/customers/customers.txt";
+       
+      /* Using try-catch to read the file with Buffered wraping FileReader and throw error in case of failure */   
+      try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+          
+          String line; // Variable created to store each line of the read file.
+   
+          /* "While loop" will read each line of the file if it is not equal to null,
+           untill there is no line using readLine() */  
+          while ((line = br.readLine()) != null){
+              System.out.println(line);// Prints the line to the console.
+          }
+       // Catch will handle exceptions.
+      } catch (IOException e) {
+          // printStackTrace will print the IO exceptions if occur.
+          e.printStackTrace();
+      }
     }
     
 }
